e5078ff7d43bbe9ca5f24a0fc923cf3749306e89
Alena Lyalyushkina <pluto_16@mail.ru>
2023-09-05 17:22:55 +1000

    fix: ПР15 добавить доменные имена

--- frontend/src/utils/Api.js
+++ frontend/src/utils/Api.js
@@ -105,7 +105,7 @@ class Api {
 }
 
 const api = new Api({
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'https://api.placeapp.students.nomoredomainsicu.ru',
 });
 
 export { api };
--- frontend/src/utils/Auth.js
+++ frontend/src/utils/Auth.js
@@ -1,5 +1,4 @@
-export const BASE_URL = 'http://localhost:3000';
-// export const BASE_URL = 'https://mesto.nomoreparties.co/v1/cohort-66';
+export const BASE_URL = 'https://api.placeapp.students.nomoredomainsicu.ru';
 
 export const register = (email, password) => {
   return fetch(`${BASE_URL}/signup`, {
